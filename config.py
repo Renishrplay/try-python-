1aa09eeeb53c34af3c026993cdd5dc20c1f6dbaa
Rplay <88985252+Renishrplay@users.noreply.github.com>
2025-01-16 09:44:36 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 
-API_ID = int(os.environ.get("API_ID", "523894"))
-API_HASH = os.environ.get("API_HASH", "e15ad5314295f")
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "683894700450:AAGtmIuSJgmjbkbi3vs")
+API_ID = int(os.environ.get("API_ID", "27812514"))
+API_HASH = os.environ.get("API_HASH", "0bee4605a7c508d58c1bcbaacf7e6314")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "5982254345:AAGvE2aHhIAi34ceWcWMwfMPgPJ-p7rvlp0")
 CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", "<b>{file_name}</b>\n\n<b>JOIN 💎 : @M2LINKS</b>")
